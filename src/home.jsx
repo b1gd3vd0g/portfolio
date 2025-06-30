8f69eb52c9f0838a65b0f3d3ca2abc8c057c41b8
b1gd3vd0g <devin.peevy@outlook.com>
2025-06-30 13:37:59 -0700

    Create a hook at the top of the page to grab the attention of visitors. It has an animation of me waving, and a short greeting/intro

--- src/home.jsx
+++ src/home.jsx
@@ -1,10 +1,35 @@
 import Header from './header';
+import Animation from './reusable/animation';
+import Vector2 from './util/vector2';
 
 function HomePage() {
+  // Configure the Image for me waving!
+  const waving = new Image();
+  waving.addEventListener('load', () => console.log('loaded'));
+  waving.addEventListener('error', () => console.log('error'));
+  waving.src = '/src/assets/bdd_waving.png';
+
   return (
-    <div className='min-h-screen bg-[#ddd]'>
-      <h1 className='text-3xl font-bold underline'>Devin Peevy</h1>
-    </div>
+    <>
+      <div className='size-fit flex m-auto p-[10px]'>
+        <div className='flex flex-col items-center justify-center'>
+          <h1 className='font-bold text-3xl underline mb-[1em]'>
+            Hi, I'm Devin!
+          </h1>
+          <p className='max-w-[500px] tracking-wide text-xl text-center'>
+            I turn creative ideas into games, apps, and interactive experiences
+            you can click, play, or deploy.
+          </p>
+        </div>
+        <Animation
+          spritesheet={waving}
+          ssPos={new Vector2(0, 0)}
+          ssSize={new Vector2(416, 744)}
+          fps={8}
+          frameCount={4}
+        />
+      </div>
+    </>
   );
 }
 
