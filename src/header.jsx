0a37df45e5e9402bf86393db21eef474e903ad96
b1gd3vd0g <devin.peevy@outlook.com>
2025-06-29 23:39:24 -0700

    A header component to be shown at the top of every page.

---
+++ src/header.jsx
@@ -0,0 +1,30 @@
+import { Link } from 'react-router-dom';
+import sprite from './assets/bdd_sprite.png';
+import text from './assets/bdd_text.png';
+
+function Header() {
+  return (
+    <header className='flex items-center h-[100px] bg-[#98ff98]'>
+      <img
+        src={sprite}
+        className='h-3/4'
+        style={{ imageRendering: 'pixelated' }}
+      />
+      <img
+        src={text}
+        className='h-1/2'
+        style={{ imageRendering: 'pixelated' }}
+      />
+    </header>
+  );
+}
+
+function NavBar() {
+  return (
+    <nav>
+      <Link to='/projects'>Projects</Link>
+    </nav>
+  );
+}
+
+export default Header;
