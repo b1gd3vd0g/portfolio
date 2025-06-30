0205419c08cc3001ff1fbed5aca3057650e7f58e
b1gd3vd0g <devin.peevy@outlook.com>
2025-06-29 23:40:33 -0700

    A page that just has my name on it for now.

---
+++ src/home.jsx
@@ -0,0 +1,11 @@
+import Header from './header';
+
+function HomePage() {
+  return (
+    <div className='min-h-screen bg-[#ddd]'>
+      <h1 className='text-3xl font-bold underline'>Devin Peevy</h1>
+    </div>
+  );
+}
+
+export default HomePage;
