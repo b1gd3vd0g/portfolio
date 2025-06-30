17a7e286fc774e0a558308681628bcbf9fb2fdf1
b1gd3vd0g <devin.peevy@outlook.com>
2025-06-30 16:40:02 -0700

    Change the text under 'Hi, I'm Devin' to something a bit better (still unsure).

--- src/home.jsx
+++ src/home.jsx
@@ -7,12 +7,10 @@ function HomePage() {
     <>
       <div className='size-fit flex m-auto p-[10px]'>
         <div className='flex flex-col items-center justify-center'>
-          <h1 className='font-bold text-3xl underline mb-[1em]'>
-            Hi, I'm Devin!
-          </h1>
-          <p className='max-w-[500px] tracking-wide text-xl text-center'>
-            I turn creative ideas into games, apps, and interactive experiences
-            you can click, play, or deploy.
+          <h1 className='text-5xl mb-[0.5em] font-[vt323]'>Hi, I'm Devin!</h1>
+          <p className='max-w-[500px] tracking-wide text-xl text-center font-[vt323]'>
+            I bring great ideas to life on screen through games, apps, and
+            creative coding solutions that run in the cloud.
           </p>
         </div>
         <Animation
