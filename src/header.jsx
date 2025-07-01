646f7c70bd1844370d214dda24eef1f74288e691
b1gd3vd0g <devin.peevy@outlook.com>
2025-07-01 15:26:04 -0700

    Remove the inline styles from the header as we no longer need it due to improvements in the CSS file.

--- src/header.jsx
+++ src/header.jsx
@@ -5,16 +5,8 @@ import text from './assets/bdd_text.png';
 function Header() {
   return (
     <header className='flex items-center h-[100px] bg-[#98ff98]'>
-      <img
-        src={sprite}
-        className='h-3/4'
-        style={{ imageRendering: 'pixelated' }}
-      />
-      <img
-        src={text}
-        className='h-1/2'
-        style={{ imageRendering: 'pixelated' }}
-      />
+      <img src={sprite} className='h-3/4' />
+      <img src={text} className='h-1/2' />
     </header>
   );
 }
