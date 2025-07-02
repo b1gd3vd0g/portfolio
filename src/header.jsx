e907ec9f8cdde2a9eb7167c3bd312d254a7f448a
b1gd3vd0g <devin.peevy@outlook.com>
2025-07-01 17:00:35 -0700

    Make the logo stuff in the header link to the home page. Use the variable --mint-green instead of hardcoded hex values.

--- src/header.jsx
+++ src/header.jsx
@@ -4,9 +4,13 @@ import text from './assets/bdd_text.png';
 
 function Header() {
   return (
-    <header className='flex items-center h-[100px] bg-[#98ff98]'>
-      <img src={sprite} className='h-3/4' />
-      <img src={text} className='h-1/2' />
+    <header className='bg-[var(--mint-green)]'>
+      <Link to='/'>
+        <div className='flex items-center h-[100px]'>
+          <img src={sprite} className='h-3/4' />
+          <img src={text} className='h-1/2' />
+        </div>
+      </Link>
     </header>
   );
 }
