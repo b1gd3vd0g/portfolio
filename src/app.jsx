06280e271314f29043653d572ad2de2e890dcda7
b1gd3vd0g <devin.peevy@outlook.com>
2025-07-01 17:07:01 -0700

    Optimize the routing structure. Add the project page for the good the bad and chad to the router.

--- src/app.jsx
+++ src/app.jsx
@@ -3,20 +3,27 @@ import HomePage from './pages/home';
 import ProjectsPage from './pages/projects';
 import Header from './header';
 import FourOhFour from './pages/404';
+import GoodBadChadProjectOverview from './pages/projects/gbc';
 
 function App() {
   return (
     <div className='min-h-screen bg-[#ddd]'>
-      <Header />
-      <main className='p-[25px] font-[vt323] text-xl'>
-        <BrowserRouter>
+      <BrowserRouter>
+        <Header />
+        <main className='p-[25px] font-[vt323] text-xl'>
           <Routes>
-            <Route path='/' element={<HomePage />} />
-            <Route path='/projects' element={<ProjectsPage />} />
+            <Route index element={<HomePage />} />
+            <Route path='projects'>
+              <Route index element={<ProjectsPage />} />
+              <Route
+                path='goodbadchad'
+                element={<GoodBadChadProjectOverview />}
+              />
+            </Route>
             <Route path='*' element={<FourOhFour />} />
           </Routes>
-        </BrowserRouter>
-      </main>
+        </main>
+      </BrowserRouter>
     </div>
   );
 }
