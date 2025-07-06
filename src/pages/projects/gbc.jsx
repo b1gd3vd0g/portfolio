269230dca1e9526a11d930479e788cf826991355
b1gd3vd0g <devin.peevy@outlook.com>
2025-07-05 22:28:01 -0700

    Add a slideshow with a couple decent gameplay screenshots.

--- src/pages/projects/gbc.jsx
+++ src/pages/projects/gbc.jsx
@@ -1,7 +1,30 @@
+import { SlideObject, Slideshow } from '../../reusable/slideshow';
+import title from '/src/assets/gbc_title.png';
+import ssVillage from '/src/assets/gbc-screenshots/village.png';
+import ssLearn from '/src/assets/gbc-screenshots/learn.png';
+import ssDefend from '/src/assets/gbc-screenshots/defend.png';
+
 function GoodBadChadProjectOverview() {
   return (
     <div>
-      <h1>The Good, the Bad, and Chad!</h1>
+      <img src={title} className='m-auto' />
+      <p className='text-center'>
+        A two-dimensional platformer adventure game, with persistent saves and a
+        fully automated cloud deployment.
+      </p>
+      <Slideshow
+        slides={[
+          new SlideObject(
+            ssVillage,
+            'Spawn at home in your peaceful, boring village.'
+          ),
+          new SlideObject(
+            ssLearn,
+            'Learn to hunt with your trusty slingshot and sword.'
+          ),
+          new SlideObject(ssDefend, 'Defend your home and protect your people.')
+        ]}
+      />
     </div>
   );
 }
