7363f050052ca45f4519bdaacb474c1388cea358
b1gd3vd0g <devin.peevy@outlook.com>
2025-07-11 08:26:31 -0700

    Remove FormGroup component from this module. Add try/catch to sendMail function in order to reflect failure when CORS errors happen. Make the button only active when the form is filled in with valid data. Add formatters and/or validators to some fields to make sure I am sent valid emails.

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -1,4 +1,10 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import { FormGroup } from '../reusable/form_groups';
+import {
+  formatPhoneNumber,
+  validateEmail,
+  validatePhone
+} from '../util/contact_validators';
 
 function ContactPage() {
   const [name, setName] = useState('');
@@ -12,8 +18,19 @@ function ContactPage() {
       <h1>Let's get in touch!</h1>
       <form className='bg-[var(--mint-green)] m-auto w-[750px] max-w-1/1 p-3 rounded-2xl'>
         <FormGroup label='Name' hint='Jane Doe' setter={setName} />
-        <FormGroup label='Email' hint='janedoe@mail.com' setter={setEmail} />
-        <FormGroup label='Phone' hint='(253) 555-0987' setter={setPhone} />
+        <FormGroup
+          label='Email'
+          hint='janedoe@mail.com'
+          setter={setEmail}
+          validator={validateEmail}
+        />
+        <FormGroup
+          label='Phone'
+          hint='(123) 456 - 7890'
+          setter={setPhone}
+          formatter={formatPhoneNumber}
+          validator={validatePhone}
+        />
         <FormGroup
           label='Header'
           hint="I'm curious about..."
@@ -38,84 +55,66 @@ function ContactPage() {
   );
 }
 
-function FormGroup({ label, hint, setter, rows = 1, type = 'text' }) {
-  const id = label.toLowerCase().replace(/ /g, '_');
-  const field =
-    rows > 1 ? (
-      <textarea
-        id={id}
-        name={id}
-        placeholder={hint}
-        className='w-1/1 border resize-none'
-        onBlur={() => {
-          const value = document.querySelector(`#${id}`).value;
-          setter(value);
-        }}
-        rows={rows}
-      ></textarea>
-    ) : (
-      <input
-        type={type}
-        id={id}
-        name={id}
-        placeholder={hint}
-        className='border w-[400px]'
-        onBlur={() => {
-          const value = document.querySelector(`#${id}`).value;
-          setter(value);
-        }}
-      />
-    );
-  const flex = rows > 1 ? 'flex-col' : 'flex';
-  return (
-    <div className={`${flex} justify-between m-2 flex-wrap`}>
-      <label htmlFor={id} className='pr-2'>
-        {label}
-      </label>
-      {field}
-    </div>
-  );
-}
-
 function SendButton({ name, email, phone, header, message }) {
   const API_HOST = 'https://www.api.bigdevdog.com';
 
+  // Has the message been sent?
   const [feedback, setFeedback] = useState({ mood: 0, message: '' });
 
+  // Can the button be clicked?
+  const [active, setActive] = useState(false);
+
+  // Set the button as active if all fields are valid and a message has not
+  // been sent already.
+  useEffect(() => {
+    if (name && email && phone && header && message && feedback.mood <= 0)
+      setActive(true);
+    else setActive(false);
+  }, [name, email, phone, header, message, feedback.mood]);
+
+  /** Attempt to sent the email if the button is active. */
   const sendMail = async () => {
-    const response = await fetch(`${API_HOST}/contact`, {
-      method: 'POST',
-      body: JSON.stringify({
-        name,
-        email,
-        phone,
-        header,
-        message
-      })
-    });
-    switch (response.status) {
-      case 200:
-        setFeedback({ mood: 1, message: 'Message sent successfully!' });
-        break;
-      default:
-        setFeedback({
-          mood: -1,
-          message: 'Message could not send. Try again later.'
-        });
+    if (!active) return;
+    try {
+      const response = await fetch(`${API_HOST}/contact`, {
+        method: 'POST',
+        body: JSON.stringify({
+          name,
+          email,
+          phone,
+          header,
+          message
+        })
+      });
+
+      switch (response.status) {
+        case 200:
+          setFeedback({ mood: 1, message: 'Message sent successfully!' });
+          break;
+        default:
+          setFeedback({
+            mood: -1,
+            message: 'Message could not send. Try again later.'
+          });
+      }
+    } catch (error) {
+      console.log(error);
+      setFeedback({ mood: -1, message: 'Fetch failed.' });
     }
   };
 
-  const colorClass = feedback.mood < 0 ? 'text-[#66130a]' : '';
+  const fbColor = feedback.mood < 0 ? 'text-[var(--dark-red)]' : '';
+  const buttonFilter = active ? 'cursor-pointer' : 'opacity-50 cursor-default';
 
   return (
     <>
       <p
         onClick={sendMail}
-        className='cursor-pointer text-2xl bg-[var(--mint-accent)] size-fit px-2 py-1 rounded-lg m-auto'
+        className={`${buttonFilter} text-2xl bg-[var(--mint-accent)] size-fit px-2 py-1 rounded-lg m-auto`}
       >
         Send mail
       </p>
-      <p className={`${colorClass}`}>{feedback.message}</p>
+      <p className={`${fbColor}`}>{feedback.message}</p>
     </>
   );
 }
