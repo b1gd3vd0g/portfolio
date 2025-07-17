1a3c5652172c26c939fe6ae6eafba013c6ee1c4e
b1gd3vd0g <devin.peevy@outlook.com>
2025-07-17 15:53:18 -0700

    Improve significantly the JSDoc comment, including taking out the old name `SimpleFormGroup`.

--- src/reusable/form_groups.jsx
+++ src/reusable/form_groups.jsx
@@ -1,23 +1,29 @@
 import { useState } from 'react';
 
 /**
- * A SimpleFormGroup is simply a label and an input field. It can be passed a `setter`
- * so that it can update state variables belonging to parent components.
- *
- * @param {object} props The props object.
+ * A Form Group has built in functionality to ensure valid data is passed
+ * through to our API. Whenever its value changes, it will **format** the data
+ * to look a certain way. Whenever it loses focus, it will first **validate**
+ * the input to make sure it fits minimum requirements. *If* the input is
+ * considered valid, it will then **set** the externally defined state variable.
+ * If the input is not considered valid, it will highlight the field in red, and
+ * the state variable will be set back to the empty string.
+ * @param {object} props
  * @param {string} props.label The label to the input field.
  * @param {string} props.hint [OPTIONAL] The placeholder for the input field.
  * @param {number} props.rows The amount of rows to be visible in the text
  * field. `DEFAULT: 1`
  * @param {string} props.type The type of input field to display. This is
- * **critical** for password fields. `DEFAULT: 'text'`
+ * **critical** for password fields. This prop is ignored for multiline fields.
+ * `DEFAULT: 'text'`
  * @param {(v: string) => void} props.setter The setter for the state variable
  * that this field is going to update on blur.
  * @param {(v: string) => string} props.formatter [OPTIONAL] The formatter for
  * the input, which will run on every change to the field's value. If ignored,
  * the input will not be formatted.
  * @param {(v: string) => boolean} props.validator [OPTIONAL] The validator for
- * the input, which will be run on blur.
+ * the input, which will be run on blur. If ignored, any non-empty input is
+ * considered valid.
  */
 export function FormGroup({
   label,
